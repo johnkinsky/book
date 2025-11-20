19947cae66a5f2afae43c039d66d0dd5e14c3349
JMK <john.kinsky@comcast.net>
2025-11-20 17:44:11 -0600

    Add GA tracking

---
+++ docs/source/_static/jmk_gtag.js
@@ -0,0 +1,5 @@
+// jmk_gtag.js - Avoids customizing the layout.html file in the theme.
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-929RXPVCFH'); 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Technical Writing - From Basics to a Complex Systems Approach'
 copyright = '2025, John M. Kinsky'
 author = 'John Kinsky'
@@ -15,24 +9,27 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('./.'))  # Path to project root (docs/source)  
 
-
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ['sphinxawesome_theme']
-
+extensions = [
+       'sphinx_book_theme',
+       'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 # https://sphinx-themes.org/#themes
 
-html_theme = "sphinxawesome_theme"
+html_theme = "sphinx_book_theme"
 html_baseurl = "https://johnkinsky.github.io/book/"
 html_static_path = ['_static'] # Path to HTML elements
 html_logo = '_static/logo.png' # Path to logo file
 html_use_index = False # Do not generate the index page
 
+# Inject GA snippet via js script
+html_js_files = [
+    ('https://www.googletagmanager.com/gtag/js?id=id=G-929RXPVCFH', {'async': 'async'}),
+    'jmk_gtag.js',
+]
