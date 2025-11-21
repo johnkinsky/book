104b9ca4ffc9e2b364b768264a85072964b826b4
JMK <john.kinsky@comcast.net>
2025-11-21 08:40:05 -0600

    Add github link to theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
-#
+
+# conf.py for book/guide Sphinx builds.
 project = 'Technical Writing - From Basics to a Complex Systems Approach'
 copyright = '2025, John M. Kinsky'
 author = 'John Kinsky'
@@ -7,28 +7,28 @@ version = '1.0'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./.'))  # Path to project root (docs/source)  
 
-# -- General configuration ---------------------------------------------------
-extensions = [
-       'sphinx_book_theme',
-       'myst_parser'
-]
+# Path to project (docs/source)  
+sys.path.insert(0, os.path.abspath('./.'))  
 
+# -- General Settings -------------------
+extensions = ['sphinx_book_theme','myst_parser']
 templates_path = ['_templates']
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-# https://sphinx-themes.org/#themes
+# -- HTML Output ------------------------
 
 html_theme = "sphinx_book_theme"
 html_baseurl = "https://johnkinsky.github.io/book/"
 html_static_path = ['_static'] # Path to HTML elements
 html_logo = '_static/logo.png' # Path to logo file
 html_use_index = False # Do not generate the index page
+html_theme_options = {
+    "repository_url": "https://github.com/johnkinsky/book/",
+    "use_repository_button": True
+}
 
-# Inject GA snippet via js script
+# -- Inject GA snippet reference---------
 html_js_files = [
     ('https://www.googletagmanager.com/gtag/js?id=id=G-929RXPVCFH', {'async': 'async'}),
     'jmk_gtag.js',
